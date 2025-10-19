e3658c47776b5b2a08831ecf8e1a6149ad0c393b
Alistair Heath <alistair.heath@hotmail.com>
2025-10-19 01:39:04 +0100

    Set example environment.

--- src/environment/example.environment.ts
+++ src/environment/example.environment.ts
@@ -1,6 +1,31 @@
-export const environment: any = {
+const environment: any = {
   models: {
     baseURL: "http://localhost",
-    port: 11434
+    port: 11434,
+    apiKey: "dummy",
+    chargePer1KTokens: {
+      default: 0.000001,
+      "gpt-oss:20b": 0.000002,
+    }
+  },
+  algorand: {
+    addr: "YOUR_ALGORAND_ADDRESS_HERE",
+    mnemonic: "YOUR_ALGORAND_MNEMONIC_HERE",
+    client: {
+      address: "https://mainnet-api.algonode.cloud/",
+      port: 443,
+      token: ""
+    },
+    paymentAssetId: 31566704
+  },
+  api: {
+    bearerAuthentication: true,
+    keys: [
+      "sk-testkey1",
+      "sk-testkey2"
+    ],
+    port: 8181
   }
 }
+
+export default environment; 
