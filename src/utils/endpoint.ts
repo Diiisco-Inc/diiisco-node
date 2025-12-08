6cd59e9f0e9752d730ee85b760963b2319b7591e
Alistair Heath <alistair.heath@hotmail.com>
2025-12-08 11:39:41 +0000

    Fixes API key leak error and returns API not found errors in the same format as OpenAI endpoints.

--- src/utils/endpoint.ts
+++ src/utils/endpoint.ts
@@ -13,9 +13,23 @@ const allowed = new Set(environment.api.keys.map((s: string) => s.trim()).filter
 /** Bearer auth middleware */
 export function requireBearer(req: any, res: any, next: any) {
   const token = parseBearer(req);
-  if (!token) return res.status(401).json({ error: "missing_bearer_token" });
+  if (!token) return res.status(401).json({
+    "error": {
+      "message": "You must provide an API key.",
+      "type": "authentication_error",
+      "param": null,
+      "code": null
+    }
+  });
 
-  if (!allowed.has(token)) return res.status(403).json({ error: "invalid_token", allowed: JSON.stringify([...allowed]) });
+  if (!allowed.has(token)) return res.status(401).json({
+    "error": {
+      "message": "Incorrect API key provided: " + token,
+      "type": "authentication_error",
+      "param": null,
+      "code": null
+    }
+  });
 
   // attach auth info for downstream handlers
   req.auth = { token, type: "opaque" };
