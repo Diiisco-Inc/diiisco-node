45383d786e29635e475059f814ed5817924212de
Alistair Heath <alistair.heath@hotmail.com>
2026-01-06 13:19:23 +0000

    Add addModel method for compiling list of available models.

--- src/utils/models.ts
+++ src/utils/models.ts
@@ -4,17 +4,22 @@ import tokenizer from "llama-tokenizer-js";
 import { ChatCompletionMessageParam } from "openai/resources/chat/completions";
 import { logger } from './logger';
 import { Environment } from "../environment/environment.types";
+import { Model } from "openai/resources/index";
+import EventEmitter from "events";
 
 export class OpenAIInferenceModel {
   openai: OpenAI;
   private env: Environment;
+  nodeEventEmitter: EventEmitter;
+  availableModels: Model[] = [];
 
-  constructor(baseURL: string) {
+  constructor(baseURL: string, nodeEvents: EventEmitter) {
     this.env = environment;
     this.openai = new OpenAI({
       baseURL: baseURL,
       apiKey: this.env.models.apiKey
     });
+    this.nodeEventEmitter = nodeEvents;
   }
 
   async getResponse(model: string, messages: ChatCompletionMessageParam[]): Promise<OpenAI.Chat.Completions.ChatCompletion> {
@@ -40,5 +45,23 @@ export class OpenAIInferenceModel {
       const tokens = tokenizer.encode(input);
       return acc + tokens.length;
     }, 0);
+  }
+
+  async addModel(models: Model[]) {
+
+    if (this.availableModels.length === 0) {
+      this.availableModels = models;
+      setTimeout(() => {
+        const uniqueModels = this.availableModels.filter((model, index, self) => 
+          index === self.findIndex((m) => m.id === model.id)
+        );
+        this.nodeEventEmitter.emit(`model-list-compiled`, uniqueModels);
+        logger.info(`✅ Model list compiled and event emitted: ${JSON.stringify(uniqueModels)}`);
+        this.availableModels = [];
+      }, environment.quoteEngine.waitTime || 5000);
+    } else {
+      this.availableModels = [...this.availableModels, ...models];
+    }
+    
   }
 }
